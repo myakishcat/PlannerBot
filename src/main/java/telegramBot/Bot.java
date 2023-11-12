4d390029f9864e87c27cabf91be86c198b7177d8
myakishcat <ekatm08@mail.ru>
2023-11-12 22:12:14 +0500

    Commands help, deadlines and unknown (connection without detailed realisation)

--- src/main/java/telegramBot/Bot.java
+++ src/main/java/telegramBot/Bot.java
@@ -15,13 +15,47 @@ public class Bot extends TelegramLongPollingBot {
         String chatID = update.getMessage().getChatId().toString();
         String text = update.getMessage().getText();
 
-        SendMessage sendMessage = new SendMessage(chatID, text);
+        switch (text){
+            case "/help" -> helpCommand(chatID);
+            case "/deadlines" -> deadlinesCommand(chatID);
+//                case "/schedule" -> Command.schedule();
+//                case "/newtask" -> Command.newTask();
+//                case "/deltask" -> Command.delTask();
+//                case "/changetask" -> Command.changeTask();
+//                case "/newevent" -> Command.newEvent();
+//                case "/delevent" -> Command.delEvent();
+//                case "/changeevent" -> Command.changeEvent();
+            default -> unknownCommand(chatID);
+        }
+    }
+
+    public void helpCommand(String chatID){
+        SendMessage sendMessage = new SendMessage(chatID, "Справка о командах");
         try{
             this.execute(sendMessage);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public void deadlinesCommand(String chatID){
+        SendMessage sendMessage = new SendMessage(chatID, "Все задачи: ...");
+        try{
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void unknownCommand(String chatID){
+        SendMessage sendMessage = new SendMessage(chatID, "Я не знаю такой команды");
+        try{
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public String getBotUsername() {
         return "individual_planner_bot";
